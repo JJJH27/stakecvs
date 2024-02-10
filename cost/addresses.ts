5c976120749dcdd83d9363eabe41c6e996f5f8ca
JJJH27 <131808859+JJJH27@users.noreply.github.com>
2024-02-10 10:42:24 -0400

    Add files via upload

---
+++ cost/addresses.ts
@@ -0,0 +1,8 @@
+//export const STAKE_TOKEN_ADDRESSES = "0x442DB81f31A8AbF774d4DE3154744Ea61cFAa73f"; //lp eggswap svit and expase
+
+export const STAKE_TOKEN_ADDRESSES = "0xc26F193525AD8e5045DcaaB97817da8D3E161174"; //token coin villas
+
+export const REWARD_TOKEN_ADDRESSES = "0xc451C62eF7392068053D80c7a66883358a547E25"; //nuevo token svit
+
+export const STAKE_CONTRACT_ADDRESSES = "0xbc69b4C131a82b56434AF38b9cb78c7514D89A9F"; //nuevo contrato
+

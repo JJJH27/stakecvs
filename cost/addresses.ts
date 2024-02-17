cbd30236372c7a1b015b1841aecbed975f0f1fdb
JJJH27 <131808859+JJJH27@users.noreply.github.com>
2024-02-17 13:32:37 -0400

    Update addresses.ts

--- cost/addresses.ts
+++ cost/addresses.ts
@@ -1,5 +1,3 @@
-//export const STAKE_TOKEN_ADDRESSES = "0x442DB81f31A8AbF774d4DE3154744Ea61cFAa73f"; //lp eggswap svit and expase
-
 export const STAKE_TOKEN_ADDRESSES = "0xc26F193525AD8e5045DcaaB97817da8D3E161174"; //token coin villas
 
 export const REWARD_TOKEN_ADDRESSES = "0xc451C62eF7392068053D80c7a66883358a547E25"; //nuevo token svit
